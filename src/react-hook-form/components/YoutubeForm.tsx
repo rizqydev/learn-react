e7847d7d1323c3350881262dbef47fac5dd92a69
Rizqy <rizqy.dev@gmail.com>
2026-01-23 14:59:32 +0700

    install dev tools

--- src/react-hook-form/components/YoutubeForm.tsx
+++ src/react-hook-form/components/YoutubeForm.tsx
@@ -1,16 +1,27 @@
+import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools"
+
 export const YoutubeForm = () => {
+  const form = useForm()
+  const { register, control } = form
+
+
   return (
-    <form>
-      <label htmlFor="username">Name</label>
-      <input type="text" id="username" name="username" />
+    <>
+      <form>
+        <label htmlFor="username">Name</label>
+        <input type="text" id="username" {...register("username")} />
 
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register("email")} />
 
-      <label htmlFor="channel">Channel</label>
-      <input type="text" id="channel" name="channel" />
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register("channel")} />
 
-      <button type="submit">Submit</button>
-    </form>
+
+        <button type="submit">Submit</button>
+      </form>
+      <DevTool control={control} />
+    </>
   )
 }
