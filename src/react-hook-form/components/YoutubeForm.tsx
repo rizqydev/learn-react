fafbe05055665c1a44ea28790450e88ed8c0caeb
Rizqy <rizqy.dev@gmail.com>
2026-01-23 15:57:22 +0700

    learn form submission

--- src/react-hook-form/components/YoutubeForm.tsx
+++ src/react-hook-form/components/YoutubeForm.tsx
@@ -1,17 +1,28 @@
 import { useForm } from "react-hook-form"
 import { DevTool } from "@hookform/devtools"
+import { useEffect, useState } from "react"
 
 let renderCount = 0
 
+type FormValues = {
+  username: string
+  email: string
+  channel: string
+}
+
 export const YoutubeForm = () => {
-  const form = useForm()
-  const { register, control } = form
+  const form = useForm<FormValues>()
+  const { register, handleSubmit, control } = form
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data)
+  }
 
   renderCount++
   return (
     <>
-      <h1>Youtube Form {renderCount / 2}</h1>
-      <form>
+      <h1>Youtube Form {renderCount}</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Name</label>
         <input type="text" id="username" {...register("username")} />
 
@@ -20,7 +31,6 @@ export const YoutubeForm = () => {
 
         <label htmlFor="channel">Channel</label>
         <input type="text" id="channel" {...register("channel")} />
-
 
         <button type="submit">Submit</button>
       </form>
