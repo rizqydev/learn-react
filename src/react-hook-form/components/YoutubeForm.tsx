2901451b9fa1ba2479a8b1db83cfb3fe885126d5
Rizqy <rizqy.dev@gmail.com>
2026-01-23 15:13:12 +0700

    learn form state and rerenders

--- src/react-hook-form/components/YoutubeForm.tsx
+++ src/react-hook-form/components/YoutubeForm.tsx
@@ -1,13 +1,16 @@
 import { useForm } from "react-hook-form"
 import { DevTool } from "@hookform/devtools"
 
+let renderCount = 0
+
 export const YoutubeForm = () => {
   const form = useForm()
   const { register, control } = form
 
-
+  renderCount++
   return (
     <>
+      <h1>Youtube Form {renderCount / 2}</h1>
       <form>
         <label htmlFor="username">Name</label>
         <input type="text" id="username" {...register("username")} />
