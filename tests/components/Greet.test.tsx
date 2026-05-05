ba8dcedac95abc7845fe817e5bab075eb405bf62
Rizqy <rizqy.dev@gmail.com>
2026-05-05 10:22:31 +0700

    learn unit testing - setup vitest

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,26 @@
+import { it, expect, describe } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Greet from '../../src/components/Greet'
+import '@testing-library/jest-dom/vitest'
+
+describe('Greet', () => {
+  it('should render Hello with the name when name is provided', () => {
+
+    render(<Greet name="John" />)
+
+    const heading = screen.getByRole('heading')
+
+    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveTextContent(/john/i)
+  })
+
+
+  it('should render login button when name is not provided', () => {
+    render(<Greet />)
+
+    const button = screen.getByRole('button')
+
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveTextContent(/login/i)
+  })
+})
